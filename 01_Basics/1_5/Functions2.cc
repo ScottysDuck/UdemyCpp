c684e7084c6fd5fe1fdc5f1c4eabfe577884d6e3
ScottysDuck <117936968+ScottysDuck@users.noreply.github.com>
2022-11-14 18:03:02 +0100

    Update Functions2.cc

--- 01_Basics/1_5/Functions2.cc
+++ 01_Basics/1_5/Functions2.cc
@@ -17,7 +17,7 @@ int main()
     int my_number = 3;
     bool check = is_even(my_number);
 
-    std::cout << "Is the number even?" << std::boolalpha is_even << std::endline;
+    std::cout << "Is the number even?" << std::boolalpha is_even << std::endl;
     
     return 0;
 }
